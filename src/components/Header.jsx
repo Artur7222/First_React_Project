1aa60854c601a189d94950525dcd6c832660f82c
Ushakov Artur <ushakov.artur93@gmail.com>
2020-11-10 17:40:54 +0500

    Разбил на компонененты, первоначальная верстка

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'; 
+
+const Header = () => {
+    return (
+        <header className="header">
+            <img src="https://habrastorage.org/webt/z1/bu/fm/z1bufmx1tce1wxwjm92w7wz_7lq.png" alt="site logo"/>
+        </header>
+    );
+};
+
+export default Header; 
---
+++ src/components/Profile.jsx
@@ -0,0 +1,29 @@
+import React, { Component } from 'react'; 
+
+const Profile = () => {
+    return (
+        <div className="content">
+            <div>
+                <img src="https://itproger.com/img/news/1540394188.jpg" alt="profile wallpapper"></img>
+            </div>
+            <div>
+                <img src="https://figuya.com/uploads/product/profile_picture/9784/profile_bloodborne-figma-actionfigur-hunter-15-cm20170920-11887-1jhj0q8" alt="profile avatar"></img>
+                +Descroption
+            </div>
+            <div>
+                My posts
+                    <div>
+                        New posts
+                    </div>
+                    <div>
+                        Post 1
+                    </div>
+                    <div>
+                        Post 2
+                    </div>
+            </div>
+      </div>
+    );
+};
+
+export default Profile; 
