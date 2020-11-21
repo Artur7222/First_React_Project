c2ce05f492415482e066cf54c3a97318d86ea969
Ushakov Artur <ushakov.artur93@gmail.com>
2020-11-21 15:52:51 +0500

    Добавил возможность добавления постов и отправки сообщений через creareRef

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -8,6 +8,13 @@ const Dialogs = (props) => {
     let dialogsElements = props.state.dialogs.map(dialog => <DialogItem name={dialog.name} id={dialog.id}/>);
     let messagesElements = props.state.messages.map(message => <Message message={message.message}/>);
 
+    let newMessageElement = React.createRef();
+
+    let addPost = () => {
+        let text = newMessageElement.current.value;
+        alert(text);
+    };
+
     return (
         <div className={classes.dialogs}>
             <div className={classes.dialogs_items}>
@@ -15,6 +22,15 @@ const Dialogs = (props) => {
             </div>
             <div className={classes.messages}>
                 {messagesElements}
+                <div>
+                    <div>
+                        <textarea ref={newMessageElement}> </textarea>
+                    </div>
+                    <div>
+                        <button onClick={addPost}>Send Message
+                        </button>
+                    </div>
+                </div>
             </div>
         </div>
     );
