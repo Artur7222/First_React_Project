e9223c4877d131bd482d9d5b90ee30c19f8a95cf
Ushakov Artur <ushakov.artur93@gmail.com>
2020-11-23 16:29:58 +0500

    Добавил добавление постов, прокинул функцию через пропсы

--- src/redux/state.js
+++ src/redux/state.js
@@ -1,3 +1,5 @@
+import {rerenderEntireTree} from "../render"
+
 let state = {
     profilePage: {
         posts: [
@@ -18,6 +20,15 @@ let state = {
             {id: 3, message: 'Хорошее имя у меня'},
             {id: 4, message: 'Шнуры хочу'}]
     }
+};
+export let addPost = (postMessage) => {
+    let newPost = {
+        id: 5,
+        message: postMessage,
+        likescount: 0
+    };
+    state.profilePage.posts.push(newPost);
+    rerenderEntireTree(state);
 };
 
 export default state;
